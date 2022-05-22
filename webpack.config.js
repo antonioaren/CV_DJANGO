7a544baaee67ca17888821ff675acc1057c74208
pedro <antonioaren2@gmail.com>
2022-05-22 21:19:02 +0200

    Configured webpack in django

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./home/webpack_source/index.js",
+    },
+
+    output: {
+        path: path.resolve("./home/static/webpack_bundles/"),
+        filename: "[name].js",
+        publicPath: "/static/webpack_bundles/",
+    },
+
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js",
+        },
+        extensions: ["*", ".js", ".vue", ".json"],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "vue-loader",
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    useBuiltIns: "usage",
+                                    corejs: 3,
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "resolve-url-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "resolve-url-loader",
+                ],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|webp|ttf|otf|woff2?|eot)/i,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.vue/i,
+                use: ["vue-loader"],
+            },
+        ],
+    },
+};
