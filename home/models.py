da9dd3d8341dcce138d2ee218a298089720888ee
pedro <antonioaren2@gmail.com>
2022-05-23 02:31:59 +0200

    Adding block behavier

--- home/models.py
+++ home/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from wagtail.admin.panels import StreamFieldPanel
 
 from wagtail.core.models import Page
+from wagtail.fields import StreamField
+
+from home.blocks import HomeBlocks
 
 
 class HomePage(Page):
-    pass
+    content = StreamField(
+        HomeBlocks(),
+        null=True,
+        blank=True,
+        verbose_name="Content",
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("content")
+    ]
+
+    class Meta:
+        verbose_name = "Home page"
